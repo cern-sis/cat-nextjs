f88aa901e8245cd06c325e8f9ca38a76d17655d7
papadopan <antonios.papadopan@gmail.com>
2022-06-13 15:28:02 +0200

    ui: update pages folder

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,12 +1,14 @@
+<<<<<<< HEAD
 // import App from 'next/app'
 import { Layout } from "antd";
 import { AT_HEADER, CERN_FOOTER, CERN_TOOLBAR } from "../components";
 import "../styles/index.css";
 import "antd/dist/antd.css";
+import styles from  '../styles/index.module.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout className="layout">
+    <Layout className={styles.layout}>
       <CERN_TOOLBAR />
       <AT_HEADER />
       <Layout.Content style={{ marginTop: "45px" }}>
