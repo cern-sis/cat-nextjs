a13cba55e71354a810f878132315a15d44e2f7ce
Neodia <ricardoxavier.nunesdeoliveira@gmail.com>
2022-06-14 09:56:38 +0200

    ui: Results Pages NextJS

---
+++ ui/components/MENU.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Typography, Menu } from "antd";
+import Link from "next/link";
+
+const { Title } = Typography;
+
+function MENU() {
+  const [current, setCurrent] = useState("icon");
+
+  const handleClick = (e: any) => {
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={handleClick}
+      mode="horizontal"
+      className="menu"
+      overflowedIndicator={false}
+    >
+      <Menu.Item className="about-us" key="about-us">
+        <Link href={`/about-us`}>
+          <Title level={2} className="about-us-link">
+            About
+          </Title>
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item className="events" key="events">
+        <Link href="https://indico.cern.ch/category/72/">
+          <Title level={2} className="events-link">
+            Events
+          </Title>
+        </Link>
+      </Menu.Item>
+
+      <Menu.Item className="contact-us" key="contact-us">
+        <Link href="mailto:atc-contact@cern.ch" target="_blank">
+          <Title level={2} className="contact-us-link">
+            Contact
+          </Title>
+        </Link>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default MENU;
