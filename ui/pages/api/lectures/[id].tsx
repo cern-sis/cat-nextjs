e826ec0de69af3038975bb71452ab8110f0e38b2
Neodia <ricardoxavier.nunesdeoliveira@gmail.com>
2022-06-15 11:11:02 +0200

    Fix

--- ui/pages/api/lectures/[id].tsx
+++ ui/pages/api/lectures/[id].tsx
@@ -6,12 +6,11 @@ export default async function handler(req, res) {
   try {
     const lecture = await findLectureById(id);
     lecture
-      ? res.status(200).json(await findLectureById(id))
+      ? res.status(200).json(lecture)
       : res
           .status(404)
           .json({ message: `Lecture with id ${id} doesn't exist.` });
   } catch (error) {
-    console.log(error);
     res.status(500).json(error);
   }
 }
